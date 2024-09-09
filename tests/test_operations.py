2d87b07d7df55f1a2d7533498307323557123d54
flesm <ostrovskijan659@gmail.com>
2024-09-09 18:15:55 +0300

    changes in test_operations

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -2,7 +2,7 @@ from httpx import AsyncClient
 
 
 async def test_add_specific_operations(ac: AsyncClient):
-    response = await ac.post("/operations", json={
+    response = await ac.post("/operations/", json={
         "id": 1,
         "quantity": "25.5",
         "figi": "figi_CODE",
@@ -15,7 +15,7 @@ async def test_add_specific_operations(ac: AsyncClient):
 
 
 async def test_get_specific_operations(ac: AsyncClient):
-    response = await ac.get("/operations", params={
+    response = await ac.get("/operations/", params={
         "operation_type": "Выплата купонов",
     })
 
